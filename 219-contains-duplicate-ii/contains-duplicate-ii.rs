8bce777cdcf8bb2a24d8f5700ed6f61b87eea979
Daniel Mikaia <113358191+DMikaia@users.noreply.github.com>
2024-10-08 11:04:44 +0300

    Time: 13 ms (86.50%) | Memory: 2.9 MB (99.16%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.rs
+++ 219-contains-duplicate-ii/contains-duplicate-ii.rs
@@ -2,18 +2,16 @@ use std::collections::HashSet;
 
 impl Solution {
     pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
-        let mut window = HashSet::new();
-        let mut left: i32 = 0;
+        let k = k as usize;
+        let mut window = HashSet::with_capacity(k + 1);
 
-        for (right, e) in nums.iter().enumerate() {
-            if (right as i32) - left > k {
-                window.remove(&nums[left as usize]);
-                left += 1;
-            }
-            if window.contains(e) {
+        for (i, &num) in nums.iter().enumerate() {
+            if !window.insert(num) {
                 return true;
             }
-            window.insert(e.clone());
+            if i + 1 > k {
+                window.remove(&nums[i - k]);
+            }
         }
 
         false
