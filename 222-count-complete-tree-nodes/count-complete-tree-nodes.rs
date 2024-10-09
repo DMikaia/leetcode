60ba5f894d8364a363e92596b123dd4e2fc8fdd0
Daniel Mikaia <113358191+DMikaia@users.noreply.github.com>
2024-10-09 16:50:55 +0300

    Time: 5 ms (7.35%) | Memory: 4.7 MB (72.06%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.rs
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.rs
@@ -21,25 +21,15 @@ use std::cell::RefCell;
 impl Solution {
     pub fn count_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
         if let Some(node) = root {
-            return Self::count_current_nodes(Some(node), 0);
+            let left_node = node.borrow().left.clone();
+            let right_node = node.borrow().right.clone();
+             
+            let left_counter = Self::count_nodes(left_node); 
+            let right_counter = Self::count_nodes(right_node); 
+
+            return 1 + left_counter + right_counter;
         }
 
         0
-    }
-
-    pub fn count_current_nodes(root: Option<Rc<RefCell<TreeNode>>>, curr_val: i32) -> i32 {
-        if let Some(node) = root {
-            let value = curr_val + 1;
-
-            let left = node.borrow().left.clone();
-            let right = node.borrow().right.clone();
-
-            let left_counter = Self::count_current_nodes(left, 0);
-            let right_counter = Self::count_current_nodes(right, 0);
-
-            return value + left_counter +right_counter;
-        }
-
-        curr_val
     }
 }
