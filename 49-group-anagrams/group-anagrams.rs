5ae953f01b738adbc1d2d73b32477d0b7882579f
Daniel Mikaia <113358191+DMikaia@users.noreply.github.com>
2024-10-11 19:53:29 +0300

    Time: 8 ms (71.08%) | Memory: 5.7 MB (16.40%) - LeetSync

--- 49-group-anagrams/group-anagrams.rs
+++ 49-group-anagrams/group-anagrams.rs
@@ -2,16 +2,15 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        let mut map: HashMap<String, Vec<String>> = HashMap::new();
+        let mut map: HashMap<Vec<char>, Vec<String>> = HashMap::new();
 
         for str in strs {
-            let mut chars: Vec<char> = str.chars().collect();
-            chars.sort();
-            let sorted_str = chars.into_iter().collect();
+            let mut key: Vec<char> = str.chars().collect();
+            key.sort();
 
-            map.entry(sorted_str).or_insert(Vec::new()).push(str);
+            map.entry(key).or_insert(Vec::new()).push(str);
         }
 
-        return map.into_values().collect();
+        map.values().cloned().collect()
     }
 }
