5f10e2f505ccfcf74e3f317f7cd276156f861ffb
Daniel Mikaia <113358191+DMikaia@users.noreply.github.com>
2024-10-14 15:10:52 +0300

    Time: 12 ms (61.65%) | Memory: 3.3 MB (58.84%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.rs
+++ 238-product-of-array-except-self/product-of-array-except-self.rs
@@ -1,9 +1,11 @@
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let mut res: Vec<i32> = vec![1; nums.len()];;
+        let mut res: Vec<i32> = vec![0; nums.len()];;
 
-        for i in 1..nums.len() {
-            res[i] = res[i - 1] * nums[i - 1];
+        let mut prefix = 1;
+        for i in 0..nums.len() {
+            res[i] = prefix;
+            prefix *= nums[i];
         }
 
         let mut postfix = 1;
