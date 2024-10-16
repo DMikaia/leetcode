43057637c04278a743f4fe5fac789ddf12f2ca3c
Daniel Mikaia <113358191+DMikaia@users.noreply.github.com>
2024-10-16 16:39:32 +0300

    Time: 0 ms (100.00%) | Memory: 2.3 MB (58.02%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        let (mut left,mut right) = (0, numbers.len() - 1);
+
+        while left < right {
+            let sum = numbers[left] + numbers[right];
+
+            if sum > target {
+                right -= 1;
+            } else if sum < target {
+                left += 1;
+            } else {
+                return vec![left as i32 + 1, right as i32 + 1];
+            }
+        }
+
+        vec![]
+    }
+}
