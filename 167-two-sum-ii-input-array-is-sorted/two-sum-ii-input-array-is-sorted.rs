5ba96c37699433758979b1381d6c79f69affda9b
Daniel Mikaia <113358191+DMikaia@users.noreply.github.com>
2024-10-16 16:45:19 +0300

    Time: 1 ms (83.42%) | Memory: 2.2 MB (58.02%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.rs
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.rs
@@ -1,16 +1,19 @@
-use std::cmp::Ordering;
-
 impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
         let (mut left, mut right) = (0, numbers.len() - 1);
 
         while left < right {
-            match (numbers[left] + numbers[right]).cmp(&target) {
-                Ordering::Less => left += 1,
-                Ordering::Greater => right -= 1,
-                Ordering::Equal => return vec![(left+1) as i32, (right+1) as i32],
+            let sum = numbers[left] + numbers[right];
+
+            if sum > target {
+                right -= 1;
+            } else if sum < target {
+                left += 1;
+            } else {
+                return vec![left as i32 + 1, right as i32 + 1];
             }
         }
+
         vec![]
     }
 }
