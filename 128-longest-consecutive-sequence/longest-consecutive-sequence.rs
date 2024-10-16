bb2bd05b69d321dbd5339422041a27fb198a7c31
Daniel Mikaia <113358191+DMikaia@users.noreply.github.com>
2024-10-16 16:21:51 +0300

    Time: 18 ms (84.43%) | Memory: 3.3 MB (61.75%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.rs
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.rs
@@ -3,15 +3,10 @@ use std::cmp::max;
 
 impl Solution {
     pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
-        let mut set = HashSet::new();
-
-        for num in nums {
-            set.insert(num);
-        }
-
+        let mut set: HashSet<i32> = nums.into_iter().collect();
         let mut longest = 0;
 
-        for num in set.iter() {
+        for &num in &set {
             if !set.contains(&(num - 1)) {
                 let mut length = 1;
 
