58c57ece266e921510e27978ff4cf942a87421f9
Daniel Mikaia <113358191+DMikaia@users.noreply.github.com>
2024-10-16 16:40:50 +0300

    Time: 0 ms (100.00%) | Memory: 2.2 MB (99.47%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.rs
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.rs
@@ -1,6 +1,6 @@
 impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
-        let (mut left,mut right) = (0, numbers.len() - 1);
+        let (mut left, mut right) = (0, numbers.len() - 1);
 
         while left < right {
             let sum = numbers[left] + numbers[right];
