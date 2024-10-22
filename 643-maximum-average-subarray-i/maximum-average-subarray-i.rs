513a01f88e296da7f2475a90c8a28b65c3d0f923
Daniel Mikaia <113358191+DMikaia@users.noreply.github.com>
2024-10-22 11:55:00 +0300

    Time: 1 ms (96.95%) | Memory: 3 MB (45.08%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.rs
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.rs
@@ -1,21 +1,19 @@
+use std::cmp;
+
 impl Solution {
     pub fn find_max_average(nums: Vec<i32>, k: i32) -> f64 {
+        let mut avg: f64 = std::f64::MIN;
         let mut sum = 0;
-        let mut avg = 0.0;
-        let k = k as usize;
-        let kf = k as f64;
+        let mut start = 0;
 
-        for i in 0..k {
-            sum += nums[i];
-        }
+        for end in 0..nums.len() {
+            sum += nums[end];
 
-        avg = sum as f64 / kf;
-
-        for i in k..nums.len() {
-            sum += nums[i];
-            sum -= nums[i - k];
-
-            avg = avg.max(sum as f64 / kf);
+            if end as i32 >= k - 1 as i32 {
+                avg = avg.max(sum as f64 / k as f64);
+                sum -= nums[start];
+                start += 1;
+            }
         }
 
         avg
