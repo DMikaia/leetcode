0907a4bdd83193ba0c3e72d6503f476f832defe9
Daniel Mikaia <113358191+DMikaia@users.noreply.github.com>
2024-10-22 13:22:50 +0300

    Time: 1 ms (66.52%) | Memory: 2.2 MB (14.51%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.rs
+++ 2-add-two-numbers/add-two-numbers.rs
@@ -20,31 +20,28 @@ impl Solution {
         mut l1: Option<Box<ListNode>>, 
         mut l2: Option<Box<ListNode>>
     ) -> Option<Box<ListNode>> {
-        let mut result = Some(Box::new(ListNode::new(0)));
-        let mut tail = result.as_mut();
+        let mut head: Option<Box<ListNode>> = None;
+        let mut tail = &mut head;
         let mut carry: i32 = 0;
 
-        while l1.is_some() || l2.is_some() || carry > 0 {
+        while l1.is_some() || l2.is_some() || carry != 0 {
             let x = l1.as_ref().map_or(0, |node| node.val);
             let y = l2.as_ref().map_or(0, |node| node.val);
             let sum = carry + x + y;
 
             carry = sum / 10;
 
-            let new_node = Box::new(ListNode {
+            let new_node = tail.insert(Box::new(ListNode {
                 val: sum % 10,
                 next: None,
-            });
+            }));
 
-            if let Some(t) = tail {
-                t.next = Some(new_node);
-                tail = t.next.as_mut();
-            }
+            tail = &mut new_node.next;
 
             l1 = l1.and_then(|node| node.next);
             l2 = l2.and_then(|node| node.next);
         }
 
-        result.unwrap().next
+        head
     }
 }
