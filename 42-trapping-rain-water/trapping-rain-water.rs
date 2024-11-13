98f6aa6ff4ad6febae32f2c6f253614efa264fea
Daniel Mikaia <113358191+DMikaia@users.noreply.github.com>
2024-11-13 14:47:02 +0300

    Time: 0 ms (100.00%) | Memory: 2.2 MB (74.58%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.rs
+++ 42-trapping-rain-water/trapping-rain-water.rs
@@ -1,28 +1,22 @@
+use std::cmp::max;
+
 impl Solution {
     pub fn trap(height: Vec<i32>) -> i32 {
         let mut water = 0;
-        let mut left_max = 0;
-        let mut right_max = 0;
         let mut left = 0;
         let mut right = height.len() - 1;
+        let mut left_max = height[left];
+        let mut right_max = height[right];
 
         while left < right {
-            if height[left] < height[right] {
-                if left_max > height[left] {
-                    water += left_max - height[left];
-                } else {
-                    left_max = height[left];
-                }
-
+            if left_max < right_max {
                 left += 1;
+                left_max = max(left_max, height[left]);
+                water += left_max - height[left];
             } else {
-                if right_max > height[right] {
-                    water += right_max - height[right];
-                } else {
-                    right_max = height[right];
-                }
-
                 right -= 1;
+                right_max = max(right_max, height[right]);
+                water += right_max - height[right];
             }
         }
 
