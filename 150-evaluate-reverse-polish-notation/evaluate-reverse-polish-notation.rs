0ca9df9e9a60e94518182b4c657e3aa4eebd0d4c
Daniel Mikaia <113358191+DMikaia@users.noreply.github.com>
2024-11-14 10:46:20 +0300

    Time: 0 ms (100.00%) | Memory: 2.7 MB (31.48%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.rs
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.rs
@@ -2,37 +2,35 @@ impl Solution {
     pub fn eval_rpn(tokens: Vec<String>) -> i32 {
         let mut stack: Vec<i32> = Vec::new();
 
+        fn is_operator(s: &str) -> bool {
+            s == "+" || s == "-" || s == "*" || s == "/"
+        }
+
         for token in tokens {
-            match token.as_str() {
-                "+" => {
-                    let n_1: i32 = stack.pop().unwrap();
-                    let n_2: i32 = stack.pop().unwrap();
+            if is_operator(&token) {
+                let n_1: i32 = stack.pop().unwrap();
+                let n_2: i32 = stack.pop().unwrap();
 
-                    stack.push(n_2 + n_1);
+                match token.as_str() {
+                    "+" => {
+                        stack.push(n_2 + n_1)
+                    }
+                    "-" => {
+                        stack.push(n_2 - n_1);
+                    }
+                    "*" => {
+                        stack.push(n_2 * n_1);
+                    }
+                    "/" => {
+                        stack.push(n_2 / n_1);
+                    }
+                    _ => {}
                 }
-                "-" => {
-                    let n_1: i32 = stack.pop().unwrap();
-                    let n_2: i32 = stack.pop().unwrap();
-
-                    stack.push(n_2 - n_1);
-                }
-                "*" => {
-                    let n_1: i32 = stack.pop().unwrap();
-                    let n_2: i32 = stack.pop().unwrap();
-
-                    stack.push(n_2 * n_1);
-                }
-                "/" => {
-                    let n_1: i32 = stack.pop().unwrap();
-                    let n_2: i32 = stack.pop().unwrap();
-
-                    stack.push(n_2 / n_1);
-                }
-                _ => {
-                    let t_int: i32 = token.trim().parse().unwrap();
-                    stack.push(t_int);
-                }
+            } else {
+                let num: i32 = token.parse().unwrap();
+                stack.push(num);
             }
+
         }
 
         stack.pop().unwrap()
