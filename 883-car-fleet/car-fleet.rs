1b7464f11299cc7976d8449a69b97e196d404850
Daniel Mikaia <113358191+DMikaia@users.noreply.github.com>
2025-01-05 10:40:09 +0300

    Time: 24 ms (19.08%) | Memory: 5.5 MB (24.28%) - LeetSync

--- 883-car-fleet/car-fleet.rs
+++ 883-car-fleet/car-fleet.rs
@@ -1,16 +1,14 @@
 impl Solution {
     pub fn car_fleet(target: i32, position: Vec<i32>, speed: Vec<i32>) -> i32 {
-        let mut pair: Vec<(i32, i32)> = position.iter()
+        let mut pair: Vec<(&i32, &i32)> = position.iter()
             .zip(speed.iter())
-            .map(|(x, y)| (*x, *y))
             .collect();
         
         pair.sort_by(|a, b| b.0.cmp(&a.0));
 
         let mut stack: Vec<f32> = Vec::new();
-
         for p in pair {
-            stack.push((target - p.0) as f32 / p.1 as f32);
+            stack.push((target - p.0) as f32 / *p.1 as f32);
             if stack.len() >= 2 && stack.last() <= stack.get(stack.len() - 2) {
                 stack.pop();
             }
